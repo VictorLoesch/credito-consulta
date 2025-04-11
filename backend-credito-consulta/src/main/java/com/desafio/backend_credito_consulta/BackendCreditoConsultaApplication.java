ef55265a2ed555dae99cb8d630eabfb9f04955b2
VictorLoesch <victorsuto10@hotmail.com>
2025-04-11 00:04:21 -0300

    start backend project

---
+++ backend-credito-consulta/src/main/java/com/desafio/backend_credito_consulta/BackendCreditoConsultaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.backend_credito_consulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCreditoConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCreditoConsultaApplication.class, args);
+	}
+
+}
---
+++ backend-credito-consulta/src/test/java/com/desafio/backend_credito_consulta/BackendCreditoConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.backend_credito_consulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCreditoConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
